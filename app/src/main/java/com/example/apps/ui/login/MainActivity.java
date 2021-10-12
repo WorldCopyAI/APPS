4fe65e81075996083e142c98311b27507e5300ca
saetu0422 <junsoi0422@gmail.com>
2021-10-12 14:27:58 +0900

    sososososososososoososs

---
+++ app/src/main/java/com/example/apps/ui/login/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.apps.ui.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.apps.R;
+
+public class MainActivity extends AppCompatActivity {
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
